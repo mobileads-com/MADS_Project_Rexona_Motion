36acf931f546e2d60ba0cf2c01572aa66152d104
Ninjoe <joequah1@gmail.com>
2015-12-02 10:56:18 +0800

    initial commit

---
+++ web/portrait/js/jarevents.js
@@ -0,0 +1,11 @@
+//var jarevents = function (el, args) {
+//    this.el = el;
+//    this.event = args.event;
+//    this.runOnce = false;
+//
+//    switch (event) {
+//        case 'tilt':
+//            this.motionAround = 'LeftRight';
+//            this.degreeToCallback = 60;
+//    }
+//};
